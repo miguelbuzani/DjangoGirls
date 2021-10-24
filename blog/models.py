987730a159939b6f0462561983bb4422a77c07d6
Mike <miguelbuzani@gmail.com>
2021-10-24 05:04:00 +0100

    added css

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.conf import settings
-from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
